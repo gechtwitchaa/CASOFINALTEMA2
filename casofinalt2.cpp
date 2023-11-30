03df7f3c29c26ebabb6d267245e502c2381ad442
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-30 11:57:55 +0100

    .

--- casofinalt2.cpp
+++ casofinalt2.cpp
@@ -10,4 +10,11 @@ void load_script(const std::string& filename, bool show_script = false) {
         return;
     }
 
+    std::string line;
+    while (getline(file, line)) {
+        if (show_script) {
+            std::cout << line << std::endl;
+        }
+    }
+
 
